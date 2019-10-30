04f3ea9c2f84210f46f6565383eb92a2ffa8ebbf
Matt Pollari <mdpollari@gmail.com>
2019-10-30 16:33:43 -0500

    second query done.

--- EmployeeSQL/HW Queries.sql
+++ EmployeeSQL/HW Queries.sql
@@ -8,3 +8,14 @@ SELECT
 FROM
 	"Employees"
 	INNER JOIN "Salaries" ON "Employees".emp_no = "Salaries".emp_no
+
+   --  Work in progress
+-- List employees who were hired in 1986.
+SELECT
+	"Employees".emp_no,
+	"Employees".first_name,
+	"Employees".last_name,
+	"Employees".hire_date
+FROM
+	"Employees"
+WHERE hire_date >= date '1986-01-01' and hire_date < date '1986-12-31'
