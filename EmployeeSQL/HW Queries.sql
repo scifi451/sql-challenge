6ea4f7e6f4e9d391fde7bb75b84bcc50f1f066e8
Matt Pollari <mdpollari@gmail.com>
2019-11-01 11:47:17 -0500

    table schema files

--- EmployeeSQL/HW Queries.sql
+++ EmployeeSQL/HW Queries.sql
@@ -9,7 +9,6 @@ FROM
 	"Employees"
 	INNER JOIN "Salaries" ON "Employees".emp_no = "Salaries".emp_no
 
-   --  Work in progress
 -- List employees who were hired in 1986.
 SELECT
 	"Employees".emp_no,
@@ -18,4 +17,21 @@ SELECT
 	"Employees".hire_date
 FROM
 	"Employees"
-WHERE hire_date >= date '1986-01-01' and hire_date < date '1986-12-31'
+WHERE
+	hire_date >= date '1986-01-01'
+	AND hire_date <= date '1986-12-31'
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+SELECT
+	"Employees".first_name,
+	"Employees".last_name,
+	"Dept_manager".dept_no,
+	"Dept_manager".emp_no,
+	"Dept_manager".from_date,
+	"Dept_manager".to_date,
+	"Departments".dept_name,
+	"Departments".dept_no
+FROM
+	"Departments"
+JOIN "Dept_manager" ON "Dept_manager".dept_no = "Departments".dept_no
+JOIN "Employees" ON "Dept_manager".emp_no = "Employees".emp_no;
---
+++ EmployeeSQL/Table Schema2.sql
@@ -0,0 +1,71 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- EmployeesSQL DB schema diagram.
+
+CREATE TABLE "Dept_manager" (
+    "dept_no" varchar(7)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "Departments" (
+    "dept_no" varchar(7)   NOT NULL,
+    "dept_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "emp_no" int   NOT NULL,
+    "title" varchar(50)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" int   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(60)   NOT NULL,
+    "last_name" varchar(60)   NOT NULL,
+    "gender" varchar(3)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(7)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
